412464c2be88ab3ef18d294d29d977cc329e5f58
Dmytro Bilokha <dmytro@posteo.net>
2017-05-28 21:19:46 +0300

    Created simple console Hello World

---
+++ HelloFx/src/main/java/com/github/dmytrobilokha/HelloWorldFxApplication.java
@@ -0,0 +1,9 @@
+package com.github.dmytrobilokha;
+
+public class HelloWorldFxApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ HelloFx/src/test/java/com/github/dmytrobilokha/HelloWorldFxApplicationTest.java
@@ -0,0 +1,14 @@
+package com.github.dmytrobilokha;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class HelloWorldFxApplicationTest
+{
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}

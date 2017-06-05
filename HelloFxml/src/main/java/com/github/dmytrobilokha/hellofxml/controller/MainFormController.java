cc0fcb6f18d37c276d83e314716863ec4a6c4557
Dmytro Bilokha <dmytro@posteo.net>
2017-06-05 21:28:58 +0300

    Adapted submit button to react on Enter key

--- HelloFxml/src/main/java/com/github/dmytrobilokha/hellofxml/controller/MainFormController.java
+++ HelloFxml/src/main/java/com/github/dmytrobilokha/hellofxml/controller/MainFormController.java
@@ -1,9 +1,12 @@
 package com.github.dmytrobilokha.hellofxml.controller;
 
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.InputEvent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 import java.util.ResourceBundle;
 
@@ -17,9 +20,20 @@ public class MainFormController {
     private ResourceBundle resources;
 
     public void submitHandler(InputEvent event) {
+        if (!isEnterEvent(event))
+            return;
         String userName = name.getText();
         String hello = resources.getString("hello");
         nameLabel.setText(hello + ", " + userName);
+    }
+
+    private boolean isEnterEvent(InputEvent event) {
+        String eventName = event.getEventType().getName();
+        if ("MOUSE_CLICKED".equals(eventName))
+            return true;
+        if ("KEY_RELEASED".equals(eventName) && ((KeyEvent) event).getCode() == KeyCode.ENTER)
+            return true;
+        return false;
     }
 
 }
